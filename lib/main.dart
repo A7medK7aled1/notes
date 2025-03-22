bcde63dc98b535de500fa21214a1c0f378ecc8b7
A7medK7aled1 <akhaledgamal7@gmail.com>
2025-03-22 20:29:07 +0200

    create home view and set theme to dark

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes/views/home/home_view.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      home: HomeView(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(useMaterial3: false),
     );
   }
 }
