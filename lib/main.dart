8577cf3cf5ce4a2712dc493e0689a7255967fc3b
A7medK7aled1 <akhaledgamal7@gmail.com>
2025-03-25 22:49:57 +0200

    add hive package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:notes/views/home/home_view.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('notes box');
   runApp(const MainApp());
 }
 
