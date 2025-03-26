412f678dc67692671dccadee79a2f8b03e65ebfd
A7medK7aled1 <akhaledgamal7@gmail.com>
2025-03-26 21:12:04 +0200

    add BlocProvider for AddNotesCubit in EditNoteView and update dependencies

--- lib/views/note/edite_note_view.dart
+++ lib/views/note/edite_note_view.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:notes/manager/cubit/add_notes_cubit/add_notes_cubit.dart';
 import 'package:notes/views/note/widgets/edit_note_view_body.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class EditNoteView extends StatelessWidget {
   const EditNoteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: EditNoteBody(),
+        body: BlocProvider(
+          create: (context) => AddNotesCubit(),
+          child: EditNoteBody(),
+        ),
       ),
     );
   }
